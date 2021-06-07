ef022355fa13ba868733728dbcd6a59a64029342
linkclean <linkclean@naver.com>
2021-06-07 16:28:25 +0900

    bootstrap
    use bootstrap
    use jQuery

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,22 +1,44 @@
 console.log("Start");
 const txt = '{"employee":[' +
-    '{"name":"John", "age":"30", "sex":"male", "url":"0.html"},' +
-    '{"name":"KIM", "age":"20", "sex":"women", "url":"1.html"}, ' +
-    '{"name":"DOE", "age":"25", "sex":"male", "url":"2.html"},' +
-    '{"name":"KHAN", "age":"22", "sex":"women", "url":"3.html"}]}';
+    '{"name":"John", "age":"30", "intro":"Practice BootStrap", "url":"0.html"},' +
+    '{"name":"KIM", "age":"20", "intro":"Practice BootStrap", "url":"1.html"}, ' +
+    '{"name":"DOE", "age":"25", "intro":"Practice BootStrap", "url":"2.html"},' +
+    '{"name":"KHAN", "age":"22", "intro":"Practice BootStrap", "url":"3.html"}]}';
 const obj = JSON.parse(txt);
 
 var url = window.location.pathname;
 //url = url + "detail.html";
 console.log(url);
 
-
-
 var vi = "";
+var modal = "";
 for(var i=0; i<obj.employee.length; i++){
-    vi += '<div class="column">'+obj.employee[i].name+'<br>' +
-    obj.employee[i].age+'<br>'+'<a class="more" href='+obj.employee[i].url+'>'+'More'+'</a>'+'</div>';
+    vi += 
+    '<div class="col-xs-12 col-sm-6 col-lg-4 col-xl-3">' +
+        '<div class="card" style="width: 18rem; margin-bottom: 20px;">' +
+            '<img src="" class="card-img-top" alt="...">' +
+            '<div class="card-body">' +
+                '<h5 class="card-title">'+obj.employee[i].name+'</h5>' +
+                '<p class="card-text">'+obj.employee[i].intro+'</p>' +
+                '<a href="#" id="modalClick" onClick=modalDetail($("a").index(this)) class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">view</a>' +
+            '</div>' + 
+        '</div>' +
+    '</div>';    
 }
-//console.log(vi);
 
-document.getElementById('row').innerHTML = vi;
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#test").toggle();
+    });
+});
+
+function modalDetail(idx){
+    modal = "";
+    console.log(idx);
+    modal += '<div>Name : '+obj.employee[idx].name+'</div>' +
+    '<div>Age : '+obj.employee[idx].age+'</div>' +
+    '<div>Introduce : '+obj.employee[idx].intro+'</div>';
+    $("#modal_detail").html(modal);
+}
+
+document.getElementById('test').innerHTML = vi;
